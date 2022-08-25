82bf04e5e1d306307dc4a84a0a95b5209ebea887
Nigro Simone <nigro.simone@gmail.com>
2022-08-25 18:57:01 +0200

    docs

--- projects/ng-as/src/lib/ng-as.directive.ts
+++ projects/ng-as/src/lib/ng-as.directive.ts
@@ -1,7 +1,7 @@
 import { Directive, Input } from "@angular/core";
 
 interface NgAsContext<T> {
-  ngLet: T;
+  ngAs: T;
   $implicit: T;
 }
 
@@ -9,24 +9,11 @@ interface NgAsContext<T> {
 export class NgAsDirective<T> {
   @Input() ngAs!: T;
 
-  /** @internal */
-  public static ngLetUseIfTypeGuard: void;
-
   /**
-   * Assert the correct type of the expression bound to the `NgLet` input within the template.
-   *
-   * The presence of this static field is a signal to the Ivy template type check compiler that
-   * when the `NgLet` structural directive renders its template, the type of the expression bound
-   * to `NgLet` should be narrowed in some way. For `NgLet`, the binding expression itself is used to
-   * narrow its type, which allows the strictNullChecks feature of TypeScript to work with `NgLet`.
-   */
-  static ngTemplateGuard_ngLet: 'binding';
-
-  /**
-   * Asserts the correct type of the context for the template that `NgLet` will render.
+   * Asserts the correct type of the context for the template that `ngAs` will render.
    *
    * The presence of this method is a signal to the Ivy template type-check compiler that the
-   * `NgLet` structural directive renders its template with a specific context type.
+   * `ngAs` structural directive renders its template with a specific context type.
    */
   static ngTemplateContextGuard<T>(dir: NgAsDirective<T>, ctx: any): ctx is NgAsContext<Exclude<T, false | 0 | '' | null | undefined>> {
       return true;
