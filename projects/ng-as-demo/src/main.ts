eef163f1045478916c0a9b76b130ae59f8fbf676
Nigro Simone <nigro.simone@gmail.com>
2025-06-08 15:35:37 +0200

    feat: angular 20

--- projects/ng-as-demo/src/main.ts
+++ projects/ng-as-demo/src/main.ts
@@ -1,11 +1,11 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { AppComponent } from './app/app.component';
-import { provideExperimentalZonelessChangeDetection } from '@angular/core';
+import { provideZonelessChangeDetection } from '@angular/core';
 
 bootstrapApplication(AppComponent, {
   providers: [
     provideAnimationsAsync(),
-    provideExperimentalZonelessChangeDetection()
+    provideZonelessChangeDetection()
   ]
 })
